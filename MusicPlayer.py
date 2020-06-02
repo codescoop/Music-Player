d8a8d94033683884cf8987c4e94d5886e81f5b0d
Yogesh Yadav <yogesh7132@gmail.com>
2020-06-02 18:26:30 +0530

    Pre-release cleanup

--- MusicPlayer.py
+++ MusicPlayer.py
@@ -6,19 +6,22 @@ import tkinter.messagebox as messagebox
 import tkinter.filedialog as filedialog
 from pygame import mixer
 from mutagen.mp3 import MP3 as mp3
+from tkinter import ttk  # Normal Tkinter.* widgets are not themed!
+from ttkthemes import ThemedTk
 
 mixer.init()
 
-root = Tk()
+# root = Tk()
+root = ThemedTk(theme="arc")
 root.title("Music Player - [YYScoop.com]")
-root.iconbitmap("downloads/favicon.ico")
-root.geometry("500x400")
+root.iconbitmap("images/favicon.ico")
+root.geometry("620x380")
 
 menubar = Menu(root, bg="#ccba41")
 root.config(menu=menubar, bg="#141f30")
 
 # current_music = ""
-current_music_path = "downloads/dj.mp3"
+current_music_path = "sample/Sample Song mp3.mp3"
 pause_status = False
 stop_status = False
 mute_status = False
@@ -27,7 +30,7 @@ reset_tread = False
 audio_length = 0.0
 time_tread = threading.Thread()
 time_counter = "unactive"
-playlist_count=0
+playlist_count=1
 playlist = []
 
 def browse_file():
@@ -54,6 +57,8 @@ def browse_file():
 def aboutus():
     messagebox.showinfo("Music Player - [YYScoop.com]", "This is a Music Player created by the volunteers of YYScoop.com")
 
+def credit():
+    messagebox.showinfo("Music Player - [YYScoop.com]","Credit : App Icon made by Freepik from www.flaticon.com")
 
 def play_music():
     global current_music_path
@@ -69,7 +74,6 @@ def play_music():
 
         stop_status = False
         if current_music_path != "":
-            print("1111111111111111111111")
             mixer.music.load(current_music_path)
             mixer.music.play()
             statusLabel["text"] = "Playing Music" + " - " + os.path.basename(current_music_path)
@@ -78,7 +82,6 @@ def play_music():
             messagebox.showerror("File not found", "No file selected. Please check again")
 
     elif pause_status == True and stop_status == True:
-        print("2222222222222222222")
         stop_status = False
         pause_status = False
         mixer.music.play()
@@ -86,7 +89,6 @@ def play_music():
 
 
     else:
-        print("33333333333333333")
         mixer.music.unpause()
         statusLabel["text"] = "Music Resumed"
         stop_status = False
@@ -117,6 +119,11 @@ def pause_music():
 
 
 def adjust_vol(value):
+
+    value=round(float(value))
+    value=str(value)
+
+    value=int(value)
     vol_value = int(value) / 100
     mixer.music.set_volume(vol_value)
 
@@ -127,12 +134,10 @@ def rewind_music():
     global time_counter
     pause_status = False
     stop_status = False
-    # print("rewind",reset_tread)
+
     if time_counter == "active":
         reset_tread = True
-    # reset_tread = True
     time.sleep(2)
-    # print(reset_tread)
     play_music()
     statusLabel["text"] = "Music Rewinded"
 
@@ -144,7 +149,7 @@ def mute_music():
         # vol_value=0
         volScale.set(0)
         mute_status = True
-        # volumeBtn.configure(image=mutephoto)
+        # volumeBtn.configure(images=mutephoto)
         volumeBtn["image"] = mutephoto
         statusLabel["text"] = "Music Muted"
     else:
@@ -175,47 +180,33 @@ def dispaly_music():
 
     time_tread = threading.Thread(target=count_duration, args=(audio_length,))
     time_tread.start()
-    # start_tread()
-    # print("leaving dispaly")
 
 
 def count_duration(s_time):
+
     global time_counter
     time_counter = "active"
-
-    # time.sleep(2)
-    print("entered count_duration")
-    original_t = s_time
-    # print(original_t)
     global reset_tread
-    print("Tread Status",reset_tread)
-    while s_time:
-        # print("in while")
+
+    while s_time != 0:
         if reset_tread == True:
             reset_tread = False
-            print("killed tread & reset_tread to FALSE \n")
             break
 
         if pause_status == True:
-            print("in continue")
             continue
         else:
-            print("entered else ")
             s_min, s_sec = divmod(s_time, 60)
             format_length = "{:02d}:{:02d}".format(round(s_min), round(s_sec))
             cur_lengthlabel["text"] = "Current length - "+ format_length
             time.sleep(1)
             s_time -= 1
-            # print("exit entered ")
-    print("exit count_duration \n")
-    # time_counter = "unactive"
 
 def del_playlist():
-    print(playlist)
     selected_music = playlist_lbox.curselection()
     playlist_lbox.delete(selected_music)
     playlist.pop(selected_music[0])
-    print(playlist)
+
 
 def on_closing():
     stop_music()
@@ -224,7 +215,7 @@ def on_closing():
 
 ## GUI Section -------------------------------------
 
-submenu1 = Menu(menubar, tearoff=0,bg="#de841d")
+submenu1 = Menu(menubar, tearoff=0)
 menubar.add_cascade(label="File", menu=submenu1)
 submenu1.add_command(label="Open", command=browse_file)
 submenu1.add_command(label="Exit", command=root.destroy)
@@ -233,75 +224,102 @@ submenu1.add_command(label="Exit", command=root.destroy)
 submenu2 = Menu(menubar, tearoff=0)
 menubar.add_cascade(label="Help", menu=submenu2)
 submenu2.add_command(label="About", command=aboutus)
+submenu2.add_command(label="Credit", command=credit)
 
-statusLabel = Label(root, text="Welcome to YY Music Player", bg="#e3c85b",relief=SUNKEN, anchor=W)
+statusLabel = Label(root, text="Click on [+Add] Song to Play Music", bg="#e3c85b",relief=SUNKEN, anchor=W)
 statusLabel.pack(side=BOTTOM, fill=X)
 
 
-mainFrame = Frame(root)
-mainFrame.pack(side=LEFT)
+mainFrame = Frame(root, bg="#141f30",highlightbackground="white",highlightthickness=1)
+mainFrame.pack(side=LEFT,padx=15)
+
+## Left Frame --------------------------------------
+
+leftFrame = Frame(mainFrame, bg="#141f30")
+leftFrame.grid(row=0,column=0,padx=5,pady=5)
+
+## Top Frame (inside Left Frame) -------------------
+
+topFrame = Frame(leftFrame, bg="#141f30")
+topFrame.pack(padx=10)
+
+sampleimage_photo = PhotoImage(file="images/sample.png")
+sampleimagelabel = Label(topFrame, image=sampleimage_photo,width=100,height=100)
+sampleimagelabel.grid(rowspan=3,column=0,padx=5,pady=5,sticky=W+N)
+
+musiclabel = ttk.Label(topFrame, text="Playing - No Music file selected", width=30)
+musiclabel.grid(row=0,column=1,padx=5,pady=5)
+
+lengthlabel = ttk.Label(topFrame,text="Total length - --:--")
+lengthlabel.grid(row=1,column=1,padx=5,pady=5)
 
 
-leftFrame = Frame(mainFrame)
-leftFrame.pack(side=LEFT)
+cur_lengthlabel = ttk.Label (topFrame,text="Current length - --:--", relief=GROOVE)
+cur_lengthlabel.grid(row=2,column=1,padx=5,pady=5)
 
-musiclabel = Label(leftFrame, text="No Music file selected")
-musiclabel.pack(padx=10,pady=10)
 
-lengthlabel = Label (leftFrame,text="Total length - --:--")
-lengthlabel.pack(padx=10,pady=15)
 
-cur_lengthlabel = Label (leftFrame,text="Current length - --:--", relief=GROOVE)
-cur_lengthlabel.pack(padx=10,pady=15)
+## Mid Frame (inside Left Frame) -------------------
 
-midFrame = Frame(leftFrame)
-midFrame.pack(padx=10)
+midFrame = Frame(leftFrame, bg="#141f30")
+midFrame.pack(padx=10, pady=10)
 
-playphoto = PhotoImage(file="downloads/play64.png")
-playBtn = Button(midFrame, image=playphoto, command=play_music)
+playphoto = PhotoImage(file="images/play64.png")
+playBtn = Button(midFrame, image=playphoto, command=play_music, bg="#141f30",borderwidth=0)
 playBtn.grid(row=0,column=0,padx=5,pady=5)
 
-stopphoto = PhotoImage(file="downloads/stop64.png")
-stopBtn = Button(midFrame, image=stopphoto,bg="#446496", command=stop_music)
+stopphoto = PhotoImage(file="images/stop64.png")
+stopBtn = Button(midFrame, image=stopphoto, command=stop_music, bg="#141f30",borderwidth=0)
 stopBtn.grid(row=0,column=1,padx=5,pady=5)
 
-pausephoto = PhotoImage(file="downloads/fwd64.png")
-pauseBtn = Button(midFrame, image=pausephoto, command=pause_music)
+pausephoto = PhotoImage(file="images/fwd64.png")
+pauseBtn = Button(midFrame, image=pausephoto, command=pause_music, bg="#141f30",borderwidth=0)
 pauseBtn.grid(row=0,column=2,padx=5,pady=5)
 
-othFrame = Frame(leftFrame)
-othFrame.pack(padx=10, pady=15)
+## Bottom Frame (inside Left Frame) -------------------
 
-rewindphoto = PhotoImage(file="downloads/fwd32.png")
-rewindBtn = Button(othFrame, image=rewindphoto, command=rewind_music)
+bottomFrame = Frame(leftFrame, bg="#141f30")
+bottomFrame.pack(padx=10, pady=5)
+
+rewindphoto = PhotoImage(file="images/rewind32.png")
+rewindBtn = Button(bottomFrame, image=rewindphoto, command=rewind_music, bg="#141f30",borderwidth=0)
 rewindBtn.grid(row=0,column=0,padx=5,pady=5)
 
-mutephoto = PhotoImage(file="downloads/mute32.png")
-volumephoto = PhotoImage(file="downloads/volume32.png")
-volumeBtn = Button(othFrame, image=volumephoto, command=mute_music)
-volumeBtn.grid(row=0,column=1,padx=40,pady=5)
+mutephoto = PhotoImage(file="images/mute32.png")
+volumephoto = PhotoImage(file="images/volume32.png")
+volumeBtn = Button(bottomFrame, image=volumephoto, command=mute_music, bg="#141f30",borderwidth=0)
+volumeBtn.grid(row=0,column=1,padx=5,pady=5)
 
 
-volScale = Scale(othFrame, from_=0, to=100, orient=HORIZONTAL, command=adjust_vol)
+volScale = ttk.Scale(bottomFrame, from_=0, to=100, orient=HORIZONTAL, command=adjust_vol)
 default = 70
 volScale.set(default)
 mixer.music.set_volume(default / 100)
 volScale.grid(row=0,column=2,padx=5,pady=5)
 
+## Right Frame ------------------------------------------
+
+rightFrame = Frame(mainFrame, bg="#141f30")
+rightFrame.grid(row=0,column=1,padx=5,pady=5)
 
 
-rightFrame = Frame(mainFrame)
-rightFrame.pack()
+add= ttk.Label(rightFrame,text="Add Songs to Playlist",font=("Helvetica",15,"bold"))
+add.pack()
 
-playlist_lbox=Listbox(rightFrame)
+playlist_lbox = Listbox(rightFrame, bd=1, width=35,height=12)
+current_music = os.path.basename(current_music_path)
+playlist_lbox.insert(playlist_count, current_music)
+playlist.insert(playlist_count, current_music_path)
+playlist_lbox.selection_set(0)
 playlist_lbox.pack(padx=15,pady=5)
 
-addBtn = Button(rightFrame, text="+ ADD", command=browse_file)
+addBtn = ttk.Button(rightFrame, text="+ ADD Song", command=browse_file)
 addBtn.pack(side=LEFT,padx=15)
 
-delBtn = Button(rightFrame, text="+ DEL", command=del_playlist)
+delBtn = ttk.Button(rightFrame, text="- DELETE Song", command=del_playlist)
 delBtn.pack()
 
+## -----------------------------------------------------------
 
 root.protocol("WM_DELETE_WINDOW", on_closing)
 
